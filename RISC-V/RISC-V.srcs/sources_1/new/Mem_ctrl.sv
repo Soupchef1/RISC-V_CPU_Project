8afff96f5f0202ded22304fa55148d8d729c0fb5
Ryan Karami <Soupchef1>
2026-06-05 23:38:26 -0400

    added mem controller file

---
+++ RISC-V/RISC-V.srcs/sources_1/new/Mem_ctrl.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/05/2026 10:56:22 PM
+// Design Name: 
+// Module Name: Mem_ctrl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Mem_ctrl(
+    
+
+    );
+endmodule
