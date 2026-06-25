fa39bdc4effb814fcec6f751d76682a5b143b630
soupchef1 <ryankarami1349@gmail.com>
2026-06-25 10:29:06 -0400

    finished branch mem minus startup/reset

--- RISC-V/RISC-V.srcs/sources_1/new/Branch_mem.sv
+++ RISC-V/RISC-V.srcs/sources_1/new/Branch_mem.sv
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: GOON LLC
+// Engineer: RYan
 // 
 // Create Date: 06/24/2026 07:52:08 PM
-// Design Name: 
+// Design Name: Branch memory
 // Module Name: Branch_mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Project Name: GOON-PU
+// Target Devices: FPGA
+// Tool Versions: VIVADO
+// Description: yurr
 // 
-// Dependencies: 
+// Dependencies: try finger but hole
 // 
-// Revision:
+// Revision: 5^96
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: Ben please make sure ts is right idk what I wrote
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -24,18 +24,16 @@ module Branch_mem(
 
     input logic clk, nrst,
 
-    input logic [31:0] pc_in, 
-    input logic [31:0] pc_d,
-    input logic [31:0] target,
+    input logic [31:0] pc_in, //pc from IF stage
+    input logic [31:0] pc_d, //pc of branch instruction in execute stage
+    input logic [31:0] target, //from ex_stage
 
-    input logic [4:0] MUX_en,
-    input logic pc_switch,
+    input logic [4:0] MUX_en, //straight from decode stage
+    input logic pc_switch, //from execute stage
 
     output logic [1:0] bht,
-    output logic [31:0] branch_addr,
-    input logic [55:0] ex_branch_data, //cache line for intruction in execute stage
-    output logic [55:0] branch_data //data to get pipleined tag + bht (whole line)
-
+    output logic [31:0] branch_addr
+    
     );
 
     localparam logic HIGH = 1'b1;
@@ -45,39 +43,84 @@ module Branch_mem(
     logic [6:0] wea;
     logic [9:0] addra, addrb;
     logic [55:0] dina, doutb;
+    logic [55:0] target_data;
 
     assign enb = HIGH;
     assign wea = '1;
     assign addra = pc_d[11:2];
     assign addrb = pc_in[11:2];
     assign branch_addr = doutb[31:0];
-    assign bht = (doutb[34]) ? 2'b00 : doutb[33:32];
+    assign bht = (doutb[34]) ? doutb[33:32] : 2'b00;  //just checks valid bit also so we nevr take if valid bit is zero
     assign dina = target_data;
-    assign branch_data = doutb;
 
     logic [19:0] pc_tag;
     logic [19:0] branch_tag;
+    logic [55:0] branch_data;
+    logic [55:0] branch_data_ex;
+    logic branch_en;
+    logic branch_en_ex;
 
+
+    assign branch_en = MUX_en[3];
+    assign branch_data = doutb;
     assign branch_tag = doutb[54:35];
-     
+    
+    logic tag_match;
+    logic tag_match_ex;
+    assign tag_match = (pc_tag == branch_tag);
+
     always_ff @(posedge clk, negedge nrst) begin
         if(!nrst) begin
-        //idk
+        //TODO: idk
+            // pc_tag <= '0;
+            // branch_data_ex <= '0;
+            // tag_match_ex <= '0;
+            // branch_en_ex <= '0;
         end
         else begin 
-            pc_tag <= pc_in[54:35];
+            pc_tag <= pc_in[31:12];
+
+            //piplined cache line for a certain intruction so we can write back
+            branch_data_ex <= branch_data;
+            // pipelined checks
+            tag_match_ex <= tag_match;
+            branch_en_ex <= branch_en;
         end
     end
 
-    logic [55:0] target_data;
+//write logic
+    always_comb begin
+        //TODO: idk what to do for reset or startup
+        if(!tag_match_ex & branch_en_ex & pc_switch) begin
+            //write with bht as 10
+            ena = HIGH;
+            target_data = {1'b0, pc_d[31:12], 1'b1, 2'b10, target};
+        end
+        else if(tag_match_ex & branch_en_ex) begin
+            //write depending on pc_switch
+            case (pc_switch)
+                HIGH: begin
+                    ena = HIGH;
+                    target_data = {1'b0, pc_d[31:12], 1'b1, (branch_data_ex[33:32] == 2'b11) ? 2'b11 : branch_data_ex[33:32] + 2'b01, target};
+                end
+                LOW: begin
+                    ena = HIGH;
+                    target_data = {1'b0, pc_d[31:12], 1'b1, (branch_data_ex[33:32] == 2'b00) ? 2'b00 : branch_data_ex[33:32] - 2'b01, target};
+                end
+            endcase
+        end
+        else begin
+            //dont write
+            ena = LOW;
+            target_data = '0;
+        end
+    end
 
     //write logic
-    // pc_tag = branch_tag => send out through pipeline as check signal
-    // if check is low and branch_en high and pc_switch is high, write and set bht to 10
-    // else if check is high and branch_en is high, write and +1 or -1 depending on pc_switch
+    // pc_tag = branch_tag => send out through pipeline as tag_match signal
+    // if tag_match is low and branch_en high and pc_switch is high, write and set bht to 10
+    // else if tag_match is high and branch_en is high, write and +1 or -1 depending on pc_switch
     // else don't write
-
-
 
 blk_mem_gen_1_sv Trip_balls_theorem (
   .clka(clk), // input wire clka
