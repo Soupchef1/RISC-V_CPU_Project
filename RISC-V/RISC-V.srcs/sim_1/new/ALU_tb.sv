aaab44438eab8ae3604ba0a943309d2f2ddce2e8
Ryan Karami <Soupchef1>
2026-06-27 14:20:48 -0400

    started working on ALU testbench

---
+++ RISC-V/RISC-V.srcs/sim_1/new/ALU_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/26/2026 02:45:43 PM
+// Design Name: 
+// Module Name: ALU_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_tb(
+
+    );
+endmodule
