97982fa6ae144e31776d3ed3757dacca8ed7d021
TripTayloriii <yye7xj@virginia.edu>
2026-06-30 21:09:17 -0400

    forwarding unit

---
+++ RISC-V/RISC-V.srcs/sources_1/new/forwardingUnit.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/30/2026 09:05:29 PM
+// Design Name: 
+// Module Name: forwardingUnit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module forwardingUnit(
+
+    );
+endmodule
