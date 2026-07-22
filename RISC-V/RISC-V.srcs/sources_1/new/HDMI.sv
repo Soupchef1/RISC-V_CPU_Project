f875d5f0740daa03af36d0a62ec3077ecbb978de
Ryan Karami <Soupchef1>
2026-07-22 02:32:05 -0400

    wrote tmds encoder for HDMI

---
+++ RISC-V/RISC-V.srcs/sources_1/new/HDMI.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/20/2026 05:42:30 PM
+// Design Name: 
+// Module Name: HDMI
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module HDMI(
+    input logic pixel_clk,
+    input logic serial_clk,
+
+    input logic vid_active_video,
+    input logic [31:0] vid_data,
+    input logic hsync,
+    input
+
+    );
+endmodule
